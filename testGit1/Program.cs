9baa4489ecd091b3d0f69168e82b0101e920529c
Administrator <Administrator@DESKTOP-7552QIC>
2019-06-29 23:56:12 +0300

    commit1

--- testGit1/Program.cs
+++ testGit1/Program.cs
@@ -6,9 +6,8 @@ namespace testGit1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Console.WriteLine("-------------------");
-            Console.WriteLine("-------------------");
+            Console.WriteLine("*****************");
             Console.ReadLine();
         }
     }
