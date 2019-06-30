d4e3c2a2edccd9bcec8b4b12aff8a51fd145e673
Administrator <Administrator@DESKTOP-7552QIC>
2019-06-30 20:36:02 +0300

    changes

--- testGit1/Program.cs
+++ testGit1/Program.cs
@@ -7,7 +7,7 @@ namespace testGit1
         static void Main(string[] args)
         {
             Console.WriteLine("===================");
-            Console.WriteLine("--------zzz-----------");
+            Console.WriteLine("--------yyy-----------");
             Console.WriteLine("*****************");
             Console.ReadLine();
         }
