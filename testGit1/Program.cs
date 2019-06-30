ac6395cff4b1a2f5c14e0c386b6e12bb28590751
elektroumut@outlook.com <elektroumut@outlook.com>
2019-06-30 22:48:44 +0300

    aaa

--- testGit1/Program.cs
+++ testGit1/Program.cs
@@ -7,6 +7,7 @@ namespace testGit1
         static void Main(string[] args)
         {
             Console.WriteLine("------------------");
+            Console.WriteLine("------------------");
             Console.ReadLine();
         }
     }
